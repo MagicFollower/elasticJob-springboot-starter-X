49f84a915f2d4d8914487374207fe0dc0cdc7175
MKello <webturing256@163.com>
2024-01-27 17:49:49 +0800

    方案一：分片参数使用方式优化

--- abc-elasticjob-spring-boot-demo/src/main/java/org/example/job/TestJob1.java
+++ abc-elasticjob-spring-boot-demo/src/main/java/org/example/job/TestJob1.java
@@ -12,14 +12,27 @@ public class TestJob1 implements SimpleJob {
 
     @Override
     public void execute(ShardingContext shardingContext) {
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-        System.out.printf(">>>>>>>>|%s|-111-|定时任务执行中|TestJob1.doBusiness...%n", df.format(LocalDateTime.now()));
-        switch (shardingContext.getShardingItem()) {
-            case 0:
-                System.out.println("分片0");
+        // DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        // System.out.printf(">>>>>>>>|%s|-111-|定时任务执行中|TestJob1.doBusiness...%n", df.format(LocalDateTime.now()));
+        // switch (shardingContext.getShardingItem()) {
+        //     case 0:
+        //         System.out.println("分片0");
+        //         break;
+        //     case 1:
+        //         System.out.println("分片1");
+        //         break;
+        // }
+
+        String shardingParameter = shardingContext.getShardingParameter();
+        switch (shardingParameter) {
+            case "Beijing":
+                System.out.println("-Beijing...");
                 break;
-            case 1:
-                System.out.println("分片1");
+            case "Shanghai":
+                System.out.println("--Shanghai...");
+                break;
+            case "Guangzhou":
+                System.out.println("---Guangzhou...");
                 break;
         }
     }
